f0701c4054178c50fa0cfa461a3298808052116e
unknown <tanya.ortega@udgvirtual.udg.mx>
2025-03-17 12:09:56 -0600

    Corrección rutas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,11 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceRootUrl(config('app.url')); // Evita errores en Render
-            URL::forceScheme('https'); // Fuerza HTTPS
-        }
+        //
     }
 }
-
-
